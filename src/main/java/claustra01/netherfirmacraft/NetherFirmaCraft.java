f8d3d0df7bca31f67a68719b72a13d40c3ef6388
sny430 <shinya430.g@gmail.com>
2023-02-01 12:41:35 +0900

    :seedling: create block registerer

--- src/main/java/claustra01/netherfirmacraft/NetherFirmaCraft.java
+++ src/main/java/claustra01/netherfirmacraft/NetherFirmaCraft.java
@@ -1,19 +1,21 @@
 package claustra01.netherfirmacraft;
 
+import claustra01.netherfirmacraft.block.NFCBlock;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-@Mod(NetherFirmaCraft.mod_id)
+@Mod("netherfirmacraft")
 public class NetherFirmaCraft {
 
-	public static final String mod_id = "netherfirmacraft";
+	public static final String namespace = "tfc";
 
 	public static final Logger LOGGER = LogUtils.getLogger();
 
 	public NetherFirmaCraft() {
 		final IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		NFCBlock.BLOCKS.register(bus);
 	}
 }
---
+++ src/main/java/claustra01/netherfirmacraft/block/NFCBlock.java
@@ -0,0 +1,12 @@
+package claustra01.netherfirmacraft.block;
+
+import claustra01.netherfirmacraft.NetherFirmaCraft;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class NFCBlock {
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, NetherFirmaCraft.namespace);
+
+}
